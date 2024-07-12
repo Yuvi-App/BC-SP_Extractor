49648d2e64c17ce3500e9fde3b3912f12935911f
Yuvi App <info@yuvi.app>
2024-07-12 16:03:49 -0400

    Initial

---
+++ BC-SP_Extractor/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExtract = New System.Windows.Forms.Button()
+        Me.ofdSP = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 152)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(226, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "BCFFVII ScratchPad Extractor by Yuvi V1.0"
+        '
+        'btnExtract
+        '
+        Me.btnExtract.Location = New System.Drawing.Point(82, 46)
+        Me.btnExtract.Name = "btnExtract"
+        Me.btnExtract.Size = New System.Drawing.Size(83, 66)
+        Me.btnExtract.TabIndex = 1
+        Me.btnExtract.Text = "Extract"
+        Me.btnExtract.UseVisualStyleBackColor = True
+        '
+        'ofdSP
+        '
+        Me.ofdSP.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(258, 176)
+        Me.Controls.Add(Me.btnExtract)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "ScratchPad Extractor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExtract As Button
+    Friend WithEvents ofdSP As OpenFileDialog
+End Class
---
+++ BC-SP_Extractor/Form1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.IO
+Imports Ionic.Zip
+
+Public Class Form1
+    Private Sub btnExtract_Click(sender As Object, e As EventArgs) Handles btnExtract.Click
+        Dim spfile = ""
+        If ofdSP.ShowDialog = DialogResult.OK Then
+            spfile = ofdSP.FileName
+        Else
+            Exit Sub
+        End If
+
+        Dim Offsets As New Dictionary(Of UInteger, UInteger) From {
+            {0, 56320}, 'MenuJAR, Requires Compression and data.bin
+            {56320, 71680},
+            {128000, 92160},
+            {220160, 112640},
+            {332800, 48128},
+            {380928, 8192},
+            {389120, 8192},
+            {397312, 8192},
+            {405504, 2048},
+            {407552, 2048}
+            }
+
+        'SetupDir
+        If Directory.Exists("tmp") = False Then
+            Directory.CreateDirectory("tmp")
+        End If
+        If Directory.Exists("built") = False Then
+            Directory.CreateDirectory("built")
+        End If
+
+
+        'Start Getting Files
+        Using br As BinaryReader = New BinaryReader(File.Open(spfile, FileMode.Open))
+            For Each O In Offsets
+                Dim tmpExtractName = $"tmp/{O.Key}.bin"
+                br.BaseStream.Position = O.Key
+                Dim Filedata = br.ReadBytes(O.Value)
+                'Take File and Modify it
+                If O.Key = 0 Then
+                    File.WriteAllBytes(tmpExtractName, Filedata)
+                    ExtractZIP(tmpExtractName)
+                    ExportFile(Filedata, True)
+                End If
+            Next
+        End Using
+    End Sub
+
+
+    Function ExportFile(inputbytes As Byte(), Compression As Boolean)
+        'If file requires compression, we must first decompress it, then recompress it using a new standard.
+        If Compression = True Then
+
+        End If
+    End Function
+
+
+    Function ExtractZIP(ImportFile)
+        Using zip1 As ZipFile = ZipFile.Read(ImportFile)
+            Dim e As ZipEntry
+            Dim ExtractCount = 0
+            Dim ExtractFilename As list(of string)
+            For Each e In zip1
+                e.Extract("tmp", ExtractExistingFileAction.OverwriteSilently)
+                ExtractCount +=1
+            Next
+            If ExtractCount = 1 
+        End Using
+    End Function
+End Class
