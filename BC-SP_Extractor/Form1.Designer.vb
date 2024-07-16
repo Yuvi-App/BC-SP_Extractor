20bdfa0053e6a94b53f7c69b60ead01b840d4b7a
Yuvi App <info@yuvi.app>
2024-07-16 16:51:35 -0400

    Added support for all the ff games

--- BC-SP_Extractor/Form1.Designer.vb
+++ BC-SP_Extractor/Form1.Designer.vb
@@ -25,22 +25,29 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnExtract = New System.Windows.Forms.Button()
         Me.ofdSP = New System.Windows.Forms.OpenFileDialog()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MiscToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExtractFixJarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 152)
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(12, 101)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(226, 15)
+        Me.Label1.Size = New System.Drawing.Size(253, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "BCFFVII ScratchPad Extractor by Yuvi V1.0"
+        Me.Label1.Text = "SQEXM ScratchPad Extractor/Builder by Yuvi V1.0"
         '
         'btnExtract
         '
-        Me.btnExtract.Location = New System.Drawing.Point(82, 46)
+        Me.btnExtract.Location = New System.Drawing.Point(222, 61)
         Me.btnExtract.Name = "btnExtract"
-        Me.btnExtract.Size = New System.Drawing.Size(83, 66)
+        Me.btnExtract.Size = New System.Drawing.Size(80, 23)
         Me.btnExtract.TabIndex = 1
         Me.btnExtract.Text = "Extract"
         Me.btnExtract.UseVisualStyleBackColor = True
@@ -49,15 +56,61 @@ Partial Class Form1
         '
         Me.ofdSP.FileName = "OpenFileDialog1"
         '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"BCFFVII", "BCFFVII-EXTRA", "BCFFVII-GoldenSaucer", "FFVII-Snowboarding"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 62)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(204, 23)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Choose Game SP"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MiscToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(314, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MiscToolStripMenuItem
+        '
+        Me.MiscToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExtractFixJarToolStripMenuItem})
+        Me.MiscToolStripMenuItem.Name = "MiscToolStripMenuItem"
+        Me.MiscToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.MiscToolStripMenuItem.Text = "Misc"
+        '
+        'ExtractFixJarToolStripMenuItem
+        '
+        Me.ExtractFixJarToolStripMenuItem.Name = "ExtractFixJarToolStripMenuItem"
+        Me.ExtractFixJarToolStripMenuItem.Size = New System.Drawing.Size(145, 22)
+        Me.ExtractFixJarToolStripMenuItem.Text = "Extract Fix.Jar"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(258, 176)
+        Me.ClientSize = New System.Drawing.Size(314, 125)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.btnExtract)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "ScratchPad Extractor"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -66,4 +119,9 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents btnExtract As Button
     Friend WithEvents ofdSP As OpenFileDialog
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MiscToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExtractFixJarToolStripMenuItem As ToolStripMenuItem
 End Class
--- BC-SP_Extractor/Form1.vb
+++ BC-SP_Extractor/Form1.vb
@@ -1,7 +1,11 @@
 ﻿Imports System.IO
-Imports Ionic.Zip
+Imports System.Text
+Imports System.Threading
+Imports System.IO.Compression
+
 
 Public Class Form1
+    Dim JARFileLocation As String = ""
     Dim BC_FFVII_GAME_Offsets As New Dictionary(Of UInteger, UInteger) From {'key=pos value=length
             {0, 56320},       '画像データ            fix/fix2
             {56320, 71680},   'マップデータ          map
@@ -14,14 +18,25 @@ Public Class Form1
             {405504, 2048},   'Now accessing...     array1                    //This pos is never written to, only read from
             {407552, 2048}    'Now downloading...   array2                    //This pos is never written to, only read from
             }
-
+    Dim BC_FFVII_GAME_FileName As New Dictionary(Of UInteger, String) From {'key=pos value=FileName
+            {0, "fix_fix2.jar"},
+            {56320, "map.jar"},
+            {128000, "train_stage_stage_p.jar"},
+            {220160, "enemy_chara.jar"},
+            {332800, "ply.jar"},
+            {380928, "atk1.jar"},
+            {389120, "atk2.jar"},
+            {397312, "atk3.jar"},
+            {405504, "savedata1.bin"},
+            {407552, "savedata2.bin"}
+            }
     Dim BC_FFVII_EXTRA_Offsets As New Dictionary(Of UInteger, UInteger) From {'key=pos value=length
             {0, 71680},     '画像データ            btlfix
             {71680, 102400},'画像データ            img_btl/img_area
             {174080, 61440},'マップデータ          btlmap
             {235520, 51200},'ミッションデータ       btlstage
             {286720, 15360},'敵キャラクターデータ    btlchara/btlenemy
-            {302080, 70656},'キャラクターデータ      btlplay
+            {302080, 70656},'キャラクターデータ      btlply
             {372736, 8192}, 'マテリアデータ1        atk1
             {380928, 8192}, 'マテリアデータ2        atk2
             {389120, 8192}, 'マテリアデータ3        atk3
@@ -29,7 +44,20 @@ Public Class Form1
             {405504, 2048}, 'Now accessing...     array1                    //This pos is never written to, only read from
             {407552, 2048}  'Now downloading...   array2                    //This pos is never written to, only read from
             }
-
+    Dim BC_FFVII_EXTRA_FileName As New Dictionary(Of UInteger, String) From {'key=pos value=Filename
+            {0, "btlfix.jar"},
+            {71680, "img_btl_img_area.jar"},
+            {174080, "btlmap.jar"},
+            {235520, "btlstage.jar"},
+            {286720, "btlchara_btlenemy.jar"},
+            {302080, "btlply.jar"},
+            {372736, "atk1.jar"},
+            {380928, "atk2.jar"},
+            {389120, "atk3.jar"},
+            {397312, "atk4.jar"},
+            {405504, "savedata1.bin"},
+            {407552, "savedata2.bin"}
+            }
     Dim BC_FFVII_GS_Offsets As New Dictionary(Of UInteger, UInteger) From {'key=pos value=length
             {0, 51200},       'サウンドデータ     gs_fix
             {51200, 128000},  '画像データ         udezumou/superdunk/chocores
@@ -38,7 +66,26 @@ Public Class Form1
             {286720, 102400}, 'アタックデータ     chocoatk
             {389120, 20480}   'Now accessing...                             //This pos is never written to, only read from
             }
-
+    Dim BC_FFVII_GS_FileName As New Dictionary(Of UInteger, String) From {'key=pos value=filename
+            {0, "gs_fix.jar"},
+            {51200, "udezumou_superdunk_chocores.jar"},
+            {179200, "chocomap.jar"},
+            {245760, "chocobo.jar"},
+            {286720, "chocoatk.jar"},
+            {389120, "savedataarray1.bin"}
+            }
+    Dim FFVII_SB_Offsets As New Dictionary(Of UInteger, UInteger) From {
+        {0, 129},
+        {129, 4318},
+        {4447, 4318},
+        {8765, 4318}
+        }
+    Dim FFVII_SB_FileName As New Dictionary(Of UInteger, String) From {
+        {0, "0.bin"},
+        {129, "1.bin"},
+        {4447, "2.bin"},
+        {8765, "3.bin"}
+        }
 
     Private Sub btnExtract_Click(sender As Object, e As EventArgs) Handles btnExtract.Click
         Dim spfile = ""
@@ -54,43 +101,168 @@ Public Class Form1
         End If
         If Directory.Exists("built") = False Then
             Directory.CreateDirectory("built")
+            Directory.CreateDirectory("built/bcffvii")
+            Directory.CreateDirectory("built/extra")
+            Directory.CreateDirectory("built/goldensaucer")
+            Directory.CreateDirectory("built/snowboarding")
         End If
 
+        'Clear TMP
+        For Each f In Directory.GetFiles("tmp")
+            File.Delete(f)
+        Next
 
-        'Start Getting Files
-        Using br As BinaryReader = New BinaryReader(File.Open(spfile, FileMode.Open))
-            For Each O In BC_FFVII_GAME_Offsets
+        Dim SelectedSPType As Integer
+        Select Case ComboBox1.SelectedIndex
+            Case 0 'bcffvii
+                StartExtractProcess(spfile, BC_FFVII_GAME_Offsets, BC_FFVII_GAME_FileName, "built/bcffvii")
+            Case 1 'extra
+                StartExtractProcess(spfile, BC_FFVII_EXTRA_Offsets, BC_FFVII_EXTRA_FileName, "built/extra")
+            Case 2 'GS
+                StartExtractProcess(spfile, BC_FFVII_GS_Offsets, BC_FFVII_GS_FileName, "built/goldensaucer")
+            Case 3 'GS
+                StartExtractProcess(spfile, FFVII_SB_Offsets, FFVII_SB_FileName, "built/snowboarding")
+        End Select
+    End Sub
+
+    Function StartExtractProcess(SPFile As String, POSList As Dictionary(Of UInteger, UInteger), FileNameList As Dictionary(Of UInteger, String), ExportLOC As String)
+        Dim ExtractedCompressedCount = 0
+        Dim ExtractedUncomrpessedCount = 0
+        Using br As BinaryReader = New BinaryReader(File.Open(SPFile, FileMode.Open))
+            For Each O In POSList
+                'Setup Names
                 Dim tmpExtractName = $"tmp/{O.Key}.bin"
+                Dim ExtractFileName = $"{FileNameList(O.Key)}"
+                Dim ExtractFilePathName = $"{ExportLOC}/{FileNameList(O.Key)}"
+
+                'Check Ensure its not already built
+                If File.Exists(ExtractFilePathName) Then
+                    File.Delete(ExtractFilePathName)
+                End If
+
+                'Get File Data
                 br.BaseStream.Position = O.Key
                 Dim Filedata = br.ReadBytes(O.Value)
-                'Take File and Modify it
-                If O.Key = 0 Then
+
+                'Lets Check if its a JAR
+                br.BaseStream.Position = O.Key
+                If Encoding.UTF8.GetString(br.ReadBytes(2)) = "PK" Then 'Found Compressed
+                    'SaveTMP
                     File.WriteAllBytes(tmpExtractName, Filedata)
-                    ExtractZIP(tmpExtractName)
-                    ExportFile(Filedata, True)
+                    Dim ExtractedFiles As List(Of String) = ExtractZIP(tmpExtractName, True)
+                    If ExtractedFiles.Count > 0 Then
+                        CreateJAR($"{JARFileLocation} -cMf {ExtractFileName} *", "tmp")
+                        Thread.Sleep(100)
+                        If File.Exists($"tmp/{ExtractFileName}") Then
+                            File.Copy($"tmp/{ExtractFileName}", ExtractFilePathName)
+                            File.Delete($"tmp/{ExtractFileName}")
+                            For Each f In ExtractedFiles
+                                File.Delete(f)
+                            Next
+                            ExtractedCompressedCount += 1
+                        End If
+                    End If
+                Else 'No Compression
+                    File.WriteAllBytes(ExtractFilePathName, Filedata)
+                    ExtractedUncomrpessedCount += 1
                 End If
             Next
         End Using
-    End Sub
-
-
-    Function ExportFile(inputbytes As Byte(), Compression As Boolean)
-        'If file requires compression, we must first decompress it, then recompress it using a new standard.
-        If Compression = True Then
-
-        End If
+        MessageBox.Show($"Extracted and Created {vbNewLine}Compressed: {ExtractedCompressedCount}{vbNewLine}Uncompressed: {ExtractedUncomrpessedCount}")
     End Function
-
-
-    Function ExtractZIP(ImportFile)
-        Using zip1 As ZipFile = ZipFile.Read(ImportFile)
-            Dim e As ZipEntry
-            Dim ExtractCount = 0
-            Dim ExtractFilename As List(Of String)
-            For Each e In zip1
-                e.Extract("tmp", ExtractExistingFileAction.OverwriteSilently)
+    Function ExtractZIP(ImportFile As String, DeleteFile As Boolean) As List(Of String)
+        Dim ExtractCount = 0
+        Dim ExtractedFiles As List(Of String) = New List(Of String)
+        Using archive As ZipArchive = ZipFile.OpenRead(ImportFile)
+            For Each entry As ZipArchiveEntry In archive.Entries
+                Dim destinationPath As String = Path.Combine("tmp", entry.FullName)
+                Dim directoryPath As String = Path.GetDirectoryName(destinationPath)
+                If Not Directory.Exists(directoryPath) Then
+                    Directory.CreateDirectory(directoryPath)
+                End If
+                entry.ExtractToFile(destinationPath, True)
+                ExtractedFiles.Add(destinationPath)
                 ExtractCount += 1
             Next
         End Using
+        If DeleteFile = True Then
+            File.Delete(ImportFile)
+        End If
+
+        Return ExtractedFiles
     End Function
+    Function CreateJAR(InputCMD As String, WD As String)
+        Dim cmd As String = InputCMD
+        Dim workingDirectory As String = WD
+        Dim psi As New ProcessStartInfo()
+        psi.FileName = "cmd.exe"
+        psi.Arguments = "/c " & cmd
+        psi.WorkingDirectory = workingDirectory
+        psi.RedirectStandardOutput = True
+        psi.RedirectStandardError = True
+        psi.UseShellExecute = False
+        psi.CreateNoWindow = True
+        Dim process As New Process()
+        process.StartInfo = psi
+        process.Start()
+        Dim output As String = process.StandardOutput.ReadToEnd()
+        Dim [error] As String = process.StandardError.ReadToEnd()
+        process.WaitForExit()
+        Console.WriteLine("Output: " & output)
+        Console.WriteLine("Error: " & [error])
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 0
+
+        If File.Exists("jar.exe") = False Then
+            MessageBox.Show("Missing JAR.exe, Please located and put into root dir of this app and try again.")
+            Application.Exit()
+        Else
+            JARFileLocation = Application.StartupPath() + "jar.exe"
+        End If
+    End Sub
+    Private Sub ExtractFixJarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExtractFixJarToolStripMenuItem.Click
+        If ofdSP.ShowDialog = DialogResult.OK Then
+            Dim ExtractedFiles As List(Of String) = ExtractZIP(ofdSP.FileName, False)
+            If ExtractedFiles.Count > 0 Then
+                For Each f In ExtractedFiles
+                    ExtractFIXJAR(f)
+                    File.Delete(f)
+                Next
+            End If
+        End If
+    End Sub
+    Function ExtractFIXJAR(inputDatabin)
+        Using br As BinaryReader = New BinaryReader(File.Open(inputDatabin, FileMode.Open))
+            Dim FileNameCount = 0
+            Dim U1 = br.ReadUInt32
+            Dim U2 = br.ReadUInt32
+            Dim ID = br.ReadByte
+            While br.BaseStream.Position < br.BaseStream.Length
+                Dim FileLengthBA = br.ReadBytes(4)
+                Array.Reverse(FileLengthBA)
+                Dim FileLength = BitConverter.ToUInt32(FileLengthBA)
+                If FileNameCount = 86 Then
+                    br.BaseStream.Position = br.BaseStream.Position + 5
+                    FileLengthBA = br.ReadBytes(4)
+                    Array.Reverse(FileLengthBA)
+                    FileLength = BitConverter.ToUInt32(FileLengthBA)
+                End If
+
+                Dim Filedata = br.ReadBytes(FileLength)
+                Dim FileName = ""
+                If FileNameCount > 88 Then
+                    FileName = $"tmp/{FileNameCount}.bin"
+                ElseIf FileNameCount > 86 Then
+                    FileName = $"tmp/{FileNameCount}.mld"
+                Else
+                    FileName = $"tmp/{FileNameCount}.gif"
+                End If
+                File.WriteAllBytes(FileName, Filedata)
+                FileNameCount += 1
+            End While
+            MessageBox.Show($"Extracted: {FileNameCount}")
+        End Using
+    End Function
+
 End Class
